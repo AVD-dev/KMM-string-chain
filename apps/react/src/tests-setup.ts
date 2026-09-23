be1ff4822508e98fd13b11abdea062f76a6ed9d5
avalladia <e_avalla@mercadona.com>
2026-09-23 16:00:17 +0200

    test: support animation events in jsdom

--- apps/react/src/tests-setup.ts
+++ apps/react/src/tests-setup.ts
@@ -1,1 +1,8 @@
+if (!("AnimationEvent" in window)) {
+  Object.defineProperty(window, "AnimationEvent", {
+    writable: true,
+    value: Event,
+  });
+}
+
 import "@testing-library/jest-dom/vitest";
