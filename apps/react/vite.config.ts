d394e8cb721a36b05b13e3f0480e72c98e25a89c
avalladia <e_avalla@mercadona.com>
2026-09-23 11:20:17 +0200

    config(react): load jest dom

--- apps/react/vite.config.ts
+++ apps/react/vite.config.ts
@@ -1,9 +1,13 @@
 import babel from "@rolldown/plugin-babel";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), babel({ presets: [reactCompilerPreset()] })],
+  test: {
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
+  },
 });
